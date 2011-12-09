521e03b539957324d0c4279778fa83ff7da6d91e
David Avellaneda <g.avella@gmail.com>
2011-12-09 14:38:14 -0500

    adding blurrrr

--- src/monokuselecta.js
+++ src/monokuselecta.js
@@ -11,19 +11,20 @@ var MonokuSelecta = new Class({
             });
 
             //Select config
-            select.store("wrapper", styledSelecta);
+            select.store('wrapper', styledSelecta);
             select.setStyle('zIndex', 999);
             select.addClass('monoku-selecta-select')
             select.addEvent('blur', this.hideSelecta);
             select.addEvent('click', this.hideSelecta);
             select.addEvent('change', this.doSelect);
+            select.addEvent('keyup', this.doSelectByEnter);
             select.size = select.options.length;
-            styledSelecta.store("select", select);
-            if(select.get("name")){
-                styledSelecta.getChildren("input").set("name", select.get("name"));
+            styledSelecta.store('select', select);
+            if(select.get('name')){
+                styledSelecta.getChildren("input").set('name', select.get('name'));
                 //cleans the name, because the value is replaced in the input hidden
-                select.set("name", "");
-                styledSelecta.getChildren("input").set("value", select.getSelected()[0].get("value"));
+                select.set('name', '');
+                styledSelecta.getChildren("input").set('value', select.getSelected()[0].get('value'));
             }
 
             styledSelecta.inject(select,'before');
@@ -33,20 +34,29 @@ var MonokuSelecta = new Class({
     },
     
     doMonokuSelecta: function(evnt){
-        var select = this.retrieve("select");
-        select.setStyle("top", this.getCoordinates().bottom);
-        select.setStyle("left", this.getPosition().x);
+        var select = this.retrieve('select');
+        select.setStyle('top', this.getCoordinates().bottom);
+        select.setStyle('left', this.getPosition().x);
         select.focus();
     },
 
     hideSelecta: function(){
         this.style.top="-99999em";
+        this.blur();
     },
 
     doSelect: function(){
         var selected = $(this).getSelected()[0];
-        this.retrieve("wrapper").getChildren('.value')[0].innerHTML = selected.text;
-        this.retrieve("wrapper").getChildren('input')[0].set('value', selected.value);
+        this.retrieve('wrapper').getChildren('.value')[0].innerHTML = selected.text;
+        this.retrieve('wrapper').getChildren('input')[0].set('value', selected.value);
+    },
+
+    doSelectByEnter: function(evt){
+        console.log(evt.key);
+        if(evt.key == 'enter'){
+            this.style.top="-99999em";
+            this.blur();
+        }
     },
 
     getValue: function(){
